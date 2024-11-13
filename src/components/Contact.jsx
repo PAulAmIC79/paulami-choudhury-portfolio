bedc8e51ca44485790f244edec02ba49166d70ca
PAulAmIC79 <paulamichoudhury12@gmail.com>
2024-11-13 18:55:26 +0530

    changes in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,22 +41,13 @@ function refreshpage() {
     <label className="mt-4 text-xl underline">NAME:</label>
 <input type="text" name="name" placeholder=" Enter Your name" required className="p-1.5 w-80 lg:w-96 mt-2 border border-s-gray-600 rounded text-black"/>
 
-<label className="mt-4 text-xl underline">EMAIL:</label>
-<input type="text" name="email" placeholder="Enter Your Email" required className="p-1.5 w-80 lg:w-96 mt-2 border border-s-gray-600 rounded text-black" />
+<label className="mt-4 text-xl underline">EMAIL ID:</label>
+<input type="text" name="email" placeholder="Enter Your Email ID" required className="p-1.5 w-80 lg:w-96 mt-2 border border-s-gray-600 rounded text-black" />
 <label className="mt-4 text-xl underline">CONTACT NO.:</label>
 <input type="number" name="phone" placeholder="Enter Your Contact Number" required className="p-1.5 w-80 lg:w-96 mt-2 border border-s-gray-600 rounded text-black"  />
 
-<label className="mt-4 text-xl underline" > HIRE ME AS:</label>
-<select name="feedback" id="feedback" className="p-1.5 cursor-pointer text-base mt-4 bg-slate-800 text-white hover:text-xl lg:hover:bg-slate-950">
 
-<option value="Full Stack Development" >Full Stack Development</option>
-<option value="Front-end Development">Front-end Development</option>
-<option value="Backend Support">Backend Support</option>
-<option value="Designer">Web Design</option>
-
-</select>
-
-<label  className="mt-4 text-xl underline">YOUR MESSAGE</label>
+<label  className="mt-4 text-xl underline">MESSAGE</label>
 <textarea name="comments" id="comments" cols="30" rows="5" required className="p-1.5 w-80 lg:w-96 mt-2 border border-s-gray-600 rounded text-black"></textarea>
 
 
