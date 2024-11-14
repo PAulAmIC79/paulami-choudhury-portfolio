69531473277e0451fd6aba41c46bae718b5e066b
PAulAmIC79 <paulamichoudhury12@gmail.com>
2024-11-14 19:57:42 +0530

    adding changed resume

--- src/ResumeLink.jsx
+++ src/ResumeLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ResumeLink() {
     return (
-<a href="Paulami-Choudhury-CV.pdf" target="_blank" rel="noreferrer" >
+<a href="CV_PAULAMI_CHOUDHURY.pdf" target="_blank" rel="noreferrer" >
     View My Resume
 </a>
     );
