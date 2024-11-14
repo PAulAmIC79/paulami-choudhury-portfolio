1cdc972faa6865ba9f7ee7b0679d6c547a46129a
PAulAmIC79 <paulamichoudhury12@gmail.com>
2024-11-14 21:35:43 +0530

    changes in about

---
+++ src/CVLink.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CVLink = () => {
+  return (
+    <a href="paulami_choudhury_cv.pdf" target="_blank" rel="noreferrer" >
+    View My Resume
+</a>
+  )
+}
+
+export default CVLink
