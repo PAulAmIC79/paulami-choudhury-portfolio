52913a9225fb99c19ca1297443c0332cc28c59ab
PAulAmIC79 <paulamichoudhury12@gmail.com>
2024-11-14 20:52:26 +0530

    adding upgraded cv

--- src/ResumeLink.jsx
+++ src/ResumeLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ResumeLink() {
     return (
-<a href="CV_PAULAMI_CHOUDHURY.pdf" target="_blank" rel="noreferrer" >
+<a href="paulami_choudhury_cv.pdf" target="_blank" rel="noreferrer" >
     View My Resume
 </a>
     );
