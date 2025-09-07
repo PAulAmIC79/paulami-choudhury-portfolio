e37b0a363925e18f6061cbf74aa02b099b1cef8b
PAulAmIC79 <paulamichoudhury12@gmail.com>
2025-09-07 16:34:08 +0530

    latest cv added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
             </p>
             <div className="flex flex-col sm:flex-row gap-6">
               <a
-                href="Paulami-Choudhury-Resume-2025.pdf"
+                href="Paulami_Choudhury_Resume_2025.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-3 text-lg md:w-52 bg-gradient-to-r  from-purple-500 to-blue-500 border border-white lg:border-purple-500 rounded-lg font-semibold hover:bg-white/10 lg:hover:bg-purple-500/10 transition-all duration-300 hover:scale-105 hover:shadow-lg hover:shadow-white/20 lg:hover:shadow-purple-500/20 text-center text-white"
